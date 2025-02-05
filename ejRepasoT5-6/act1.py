885e232b3b74b16f15c64b918df59dd968711dcd
pabrodpen <pabrodpen@gmail.com>
2025-02-05 12:40:47 +0100

    add

--- ejRepasoT5-6/act1.py
+++ ejRepasoT5-6/act1.py
@@ -9,17 +9,19 @@ puerto = 27017
 
 try:
     # Intentar conectarse al servidor MongoDB
-    client = MongoClient(f"mongodb://{usuario}:{clave}@{host}:{puerto}/{base_datos}",
-                         serverSelectionTimeoutMS=5000)
-    
+    client = MongoClient(
+        f"mongodb://{usuario}:{clave}@{host}:{puerto}/{base_datos}",
+        serverSelectionTimeoutMS=5000,
+    )
+
     # Seleccionar la base de datos
     db = client[base_datos]
-    
+
     # Eliminar la colección "Productos" si existe
     if "Productos" in db.list_collection_names():
         db.Productos.drop()
         print("Colección 'Productos' eliminada.")
-    
+
     # Intentar acceder a la base de datos para verificar la conexión
     colecciones = db.list_collection_names()
     print("Conexión exitosa. Colecciones en la base de datos:")
@@ -38,33 +40,37 @@ try:
     coleccion_productos = db.create_collection("Productos")
     # Intentar acceder a la base de datos para verificar la conexión
     colecciones = db.list_collection_names()
-    print("Conexión exitosa. Colecciones en la base de datos después de la creación de productos:")
+    print(
+        "Conexión exitosa. Colecciones en la base de datos después de la creación de productos:"
+    )
     print(colecciones)
 
     # Creación de 3 productos
-    coleccion_productos.insert_many([
-        {
-            "nombre": "Laptop Lenovo",
-            "precio": 799.99,
-            "categoria": "Electrónica",
-            "stock": 15,
-            "descuento": 10.0
-        },
-        {
-            "nombre": "Mesa",
-            "precio": 299.99,
-            "categoria": "Mueble",
-            "stock": 25,
-            "descuento": 9.0
-        },
-        {
-            "nombre": "Lavadora",
-            "precio": 399.99,
-            "categoria": "Electrodoméstico",
-            "stock": 8,
-            "descuento": 50.0
-        }
-    ])
+    coleccion_productos.insert_many(
+        [
+            {
+                "nombre": "Laptop Lenovo",
+                "precio": 799.99,
+                "categoria": "Electrónica",
+                "stock": 15,
+                "descuento": 10.0,
+            },
+            {
+                "nombre": "Mesa",
+                "precio": 299.99,
+                "categoria": "Mueble",
+                "stock": 25,
+                "descuento": 9.0,
+            },
+            {
+                "nombre": "Lavadora",
+                "precio": 399.99,
+                "categoria": "Electrodoméstico",
+                "stock": 8,
+                "descuento": 50.0,
+            },
+        ]
+    )
 
     print("Productos:")
     productos = coleccion_productos.find()
@@ -73,19 +79,26 @@ try:
 
     print("Productos cuyo precio es mayor a 300:")
     # Limita los resultados a dos productos y ordénalos en orden descendente por precio.
-    productos = coleccion_productos.find({"precio": {"$gt": 300}}).limit(2).sort("precio", -1)
+    productos = (
+        coleccion_productos.find({"precio": {"$gt": 300}}).limit(2).sort("precio", -1)
+    )
     for producto in productos:
         print(producto)
 
     # Selecciona un producto por su nombre y actualiza su precio y stock con update_one().
-    coleccion_productos.update_one({"nombre": "Laptop Lenovo"}, {"$set": {"nombre": "Portátil Lenovo", "stock": 20}})
+    coleccion_productos.update_one(
+        {"nombre": "Laptop Lenovo"},
+        {"$set": {"nombre": "Portátil Lenovo", "stock": 20}},
+    )
     print("Productos después de la actualización de nombre y stock:")
     productos = coleccion_productos.find()
     for producto in productos:
         print(producto)
 
     # Aumenta el stock de todos los productos de la categoría "Electrónica" en 10 unidades usando update_many().
-    coleccion_productos.update_many({"categoria": "Electrónica"}, {"$inc": {"stock": 10}})
+    coleccion_productos.update_many(
+        {"categoria": "Electrónica"}, {"$inc": {"stock": 10}}
+    )
     print("Productos después de la actualización del stock:")
     productos = coleccion_productos.find()
     for producto in productos:
@@ -116,6 +129,6 @@ except Exception as err:
     print(f"Ocurrió un error inesperado: {err}")
 finally:
     # Cerrar la conexión si se estableció correctamente
-    if 'client' in locals():
+    if "client" in locals():
         client.close()
         print("Conexión cerrada.")
